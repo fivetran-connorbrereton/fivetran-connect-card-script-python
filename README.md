3ed4c43c6d50cab8daa63ef3a1d8b3dc42a8337b
Jimmy Hooker <james.hooker@fivetran.com>
2022-07-15 00:56:25 -0700

    Better error handling

--- README.md
+++ README.md
@@ -1,6 +1,30 @@
-# fivetran-connect-card-sample-python
-This repository is a Python implementation of generating a connect card using the Fivetran REST API.
+# Fivetran Connect Card Generation Script in Python
+Easily generate connect cards from the comfort of your chair using Python
 
-The purpose of this code is to show the user how to embed the APIs to accomplish the goal of creating a connect card.
+## How this works
+We're going to setup a really simple script that will generate a [Connect Card](https://fivetran.com/docs/rest-api/connectors/connect-card) URL for Google Analytics. Want to try other connector types? Check out the config examples here: https://fivetran.com/docs/rest-api/connectors/config
 
+## Prepare
+- You'll need a Fivetran account and your key and secret that you can find in settings. Find out more in our [getting started guide](https://fivetran.com/docs/rest-api/getting-started).
+- Make sure you have a [working python environment](https://xkcd.com/1987/).
+- Setup a destination in Fivetran (ie Snowflake, BigQuery, etc): https://fivetran.com/docs/destinations and get your destination ID from the bottom of the destination page details in your dashboard. 
 
+## Dev Environment
+- Make sure you have pip installed: `pip -V`
+- Make sure you have virtualenv installed: `virtualenv --v` (install with `pip install virtualenv`)
+
+## Install required packages
+- Install a virtual env for this project: `python3 -m venv .venv`
+- Activate the virtual env: `source .venv/bin/activate`
+- Install requirements: `pip install -r requirements.txt`
+
+## Create .env file
+- Create a `.env` file in the root of the project and add your key, secret, destination group id in the below format:
+```
+API_KEY=
+API_SECRET=
+GROUP_ID=
+```
+
+# Run the script
+- `python create_cc.py`
--- create_cc.py
+++ create_cc.py
@@ -4,7 +4,10 @@ import json
 import requests
 from requests.auth import HTTPBasicAuth
 import environ
+from random_word import RandomWords
+import time
 
+# Begin Auth
 # Reads the API_KEY and API_SECRET from .env file.
 # Create one in the same directory as this file.
 env = environ.Env()
@@ -18,26 +21,56 @@ headers = {
     'Authorization': 'Basic ' + API_KEY,
     'Content-Type': 'application/json'
 }
+# End Auth
+
+# Begin config
+GROUP_ID = env("GROUP_ID")
+connector_type = 'google_analytics'
+# End config
+
+## Random words for schema id
+r = RandomWords()
+schemaId = r.get_random_word() + '_' + r.get_random_word()
+# End config
 
 base_url = 'https://api.fivetran.com/v1/'
-group_id = 'wad_crewmate'
 
+# Connector Config Payload: https://fivetran.com/docs/rest-api/connectors/config
 payload = {
-    "service": "adwords", # Put in your service id
-    "group_id": "quenched_serotonin", # Put in your group_id (destination pipeline)
+    "service": connector_type,
+    "group_id": GROUP_ID,
+    "Paused": "true",
     "run_setup_tests": False,
     "config": {
-        "schema": "test_0" # Name your schema in the destination
+        "schema": schemaId
     }
 }
 
+# STDOUT colors
+cyan = '\033[96m'
+endc = '\033[0m'
+failure = '\033[91m'
+
 # Create the shell connector
-response = requests.post(base_url + 'connectors', auth=auth, json=payload).json()
+print(f"{cyan}Creating a Connector...{endc}")
+
+while True:
+    time.sleep(5) # five second retry delay
+    try:
+        response = requests.post(base_url + 'connectors', auth=auth, json=payload).json()
+        if "TooManyRequests" in response['code']:
+            print(f"{failure}Failure{endc}\n")
+            raise SystemExit(response['message'])
+            print("\n")
+        break
+    except requests.exceptions.RequestException as error:
+        print(error)
 
 # Parse out the the connector_id
 connector_id = response['data']['id']
 
 # Create the connect card token
+print(f"{cyan}Creating a Connect Card Token...{endc}\n")
 response_token = requests.post(base_url + 'connectors/' + connector_id + 
     '/' + 'connect-card-token', auth=auth, json=payload).json()
 
@@ -45,4 +78,5 @@ response_token = requests.post(base_url + 'connectors/' + connector_id +
 token = response_token['token']
 
 # Generate the connect card link
+print(f"{cyan}All done! Get your URL below:{endc}")
 print('https://fivetran.com/connect-card/setup?redirect_uri=fivetran.com&auth=' + token)
